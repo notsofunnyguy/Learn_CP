522e812237be7ff917914c8c730dc2f2222f2384
elfreki <ojas.dubey81@gmail.com>
2021-01-22 20:53:15 +0530

    ojas

--- problemset/urls.py
+++ problemset/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('register/', views.register, name='problemset-register'),
     path('problem/', views.problem, name='problemset-problem'),
     path('submit/', views.submitcode, name='problemset-submit_code'),
-    path('discussions/', views.discussions, name='problemset-discussions')
+    path('discussions/', views.discussions, name='problemset-discussions'),
+    path('ide/', views.ide, name='online-ide'),
+    path('run/', views.run, name='run-code')
 ]
--- problemset/views.py
+++ problemset/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Problem, Category
+from django.views.decorators.csrf import csrf_exempt
+import subprocess
+import os
+
 
 def home(request):
     problems = None
@@ -18,7 +22,7 @@ def home(request):
 
 def problem(request):
     problems = Problem.get_all_objects()
-    return render(request, 'problemset/problem.html',  {'problems':problems })
+    return render(request, 'problemset/problem.html', {'problems': problems})
 
 
 def submitcode(request):
@@ -31,3 +35,37 @@ def discussions(request):
 
 def register(request):
     return render(request, 'problemset/register.html')
+
+
+def ide(request):
+    return render(request, 'problemset/ide.html')
+
+
+def run(request):
+    cod = request.POST.get("cod")
+    input = request.POST.get("input")
+    print(cod)
+    file = open("problem.cpp", "w+")
+    file1 = open("input.txt","w+")
+    file.write(cod)
+    file1.write(input)
+    print(file.read())
+    print(file1.read())
+    filename = "problem.cpp"
+    runcode = subprocess.Popen(['g++', filename], stdout=subprocess.PIPE)
+    while runcode.poll() is None:
+        continue
+    ps_process = subprocess.Popen(["cat", "input.txt"], stdout=subprocess.PIPE)
+    output = subprocess.Popen(['./a.out'],stdin = ps_process.stdout, stdout=subprocess.PIPE)
+    ps_process.stdout.close()
+    file2 = open("problemset/static/problemset/output.txt","w+")
+    file2.write(output.stdout.read().decode("utf-8"))
+    #print(output.stdout.read().decode("utf-8"))
+    print("P")
+    while output.poll() is None :
+        continue
+    #if output.poll() is None :
+    return render(request, 'problemset/ide.html')
+
+
+
